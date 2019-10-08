7d72522ca937b2824353a9230d8aab170ade4d82
Aleksiel Horak <captiveCorsair@Aleksiels-MacBook-Pro.local>
2019-10-08 17:09:47 +0200

    ex00 project build

---
+++ day00/ex00/ex00/ex00/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ex00
+//
+//  Created by Aleksiel Horak on 2019/10/08.
+//  Copyright © 2019 ahorak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
