3d891d7337bf904f9c9be95baffd2c2f96fb2bbe
aleksiel HORAK <ahorak@c2r5s2.wethinkcode.co.za>
2019-10-08 20:05:50 +0200

    ex01 done

--- day00/ex01/ex01/ex01/ViewController.swift
+++ day00/ex01/ex01/ex01/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var touchMe: UIButton!
+    
+    @IBOutlet weak var harderLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        touchMe.setTitle("Touch Me", for: .normal)
+        harderLabel.text = "...Waiting"
     }
 
-
+    var flag:Int = 0
+    var text:String = "Harder!"
+    
+    @IBAction func touchedButton(_ sender: Any) {
+        if flag == 0 {
+            harderLabel.text = text
+            flag = 1
+        }
+        else {
+            text = text + " Harder!"
+            harderLabel.text = text
+        }
+    }
 }
 
