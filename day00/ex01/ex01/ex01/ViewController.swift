760e97aa5f5ef2ef1d1e7ab937707e6857db0b07
aleksiel HORAK <ahorak@c2r5s2.wethinkcode.co.za>
2019-10-08 19:34:31 +0200

    ex00 framework

---
+++ day00/ex01/ex01/ex01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ex01
+//
+//  Created by aleksiel HORAK on 2019/10/08.
+//  Copyright © 2019 ahorak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
