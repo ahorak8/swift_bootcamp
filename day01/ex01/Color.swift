e3c79f3f89651e4e2aeb83eeec17ec3bb0dfa692
aleksiel HORAK <ahorak@c1r2s2.wethinkcode.co.za>
2019-10-09 16:44:52 +0200

    ex01 start

---
+++ day01/ex01/Color.swift
@@ -0,0 +1,9 @@
+enum  Color: String {
+
+	case Spade = "black"
+	case Heart = "red"
+	case Diamond = "blue"
+	case Club = "green"
+
+	static let allColors : [Color] = [Spade, Heart, Diamond, Club]
+}
